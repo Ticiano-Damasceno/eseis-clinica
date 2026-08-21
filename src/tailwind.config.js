0e57baae917d259d933d5aed1e68dcd4cdaa8e13
Ticiano <119520017+Ticiano-Damasceno@users.noreply.github.com>
2026-08-21 20:28:38 +0000

    front setup: Removendo componentes padrão para iniciar a criação

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -12,7 +12,19 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                display: ['"Playfair Display"', 'serif'],
+                sans: ['Montserrat', 'sans-serif'],
+            },
+            colors: {
+                eseis: {
+                    yellow: '#ffde59',
+                    orange: '#eb6024',
+                    beige: '#f6dfba',
+                    tan: '#d8b38a',
+                    terracotta: '#d9874f',
+                    olive: '#828a69',
+                    brick: '#c16146',
+                },
             },
         },
     },
